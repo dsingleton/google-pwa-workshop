5150144e75b68aa4df6ce1105b663f9cac1cf9f3
David Singleton <david.singleton@digital.cabinet-office.gov.uk>
2016-03-17 11:04:45 +0000

    Test service worker registeration

--- work/app.js
+++ work/app.js
@@ -208,4 +208,9 @@
     app.saveSelectedCities();
    }
 
+   if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then(function() { console.log('service worker registered')})
+   }
 })();
